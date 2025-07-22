b86ac923e87eba501fb32111d658594d845a8e15
TianYi Wei <weity23@mails.tsinghua.edu.cn>
2025-07-22 12:46:11 +0800

    Delete build directory

--- build/CMakeFiles/qt_programming_2024.dir/src/Items/Mountable.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/qt_programming_2024.dir/src/Items/Mountable.cpp.obj: \
- C:\Users\ziiih\Desktop\Pixel-Dual\Pixel-Dual\src\Items\Mountable.cpp \
- C:\Users\ziiih\Desktop\Pixel-Dual\Pixel-Dual\src\Items\Mountable.h
